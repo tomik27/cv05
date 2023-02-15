36c874e4c337b2bb272fffbfc20ae608b52d2592
tomik27 <42244137+tomik27@users.noreply.github.com>
2023-02-15 15:16:02 +0100

    Initial commit

---
+++ src/main/java/cz/upce/fei/cv01/Cv01Application.java
@@ -0,0 +1,13 @@
+package cz.upce.fei.cv01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cv01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cv01Application.class, args);
+	}
+
+}
---
+++ src/test/java/cz/upce/fei/cv01/Cv01ApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.upce.fei.cv01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cv01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
